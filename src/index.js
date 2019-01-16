cd43751ee044b03d4a641e485c8ee93dfbe3a092
Mohit Sinha <mohitsinha.it@gmail.com>
2019-01-17 03:11:57 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+window.$ = window.jQuery = require("jquery");
+
+require('normalize.css/normalize.css');
+require('./styles/index.scss');
+require('./scripts/navbar')
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const pluginsTriggerElement = document.getElementById('plugins-trigger');
+    const pluginsElement = document.getElementById('plugins');
+
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+
+    pluginsTriggerElement.onclick = () => {
+        pluginsElement.classList.toggle(pluginsVisibleClass);
+    }
+});
